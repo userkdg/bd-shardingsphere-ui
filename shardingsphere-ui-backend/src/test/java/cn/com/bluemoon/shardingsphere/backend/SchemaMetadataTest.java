aaaafde8e8f24c6080cbb7b7244abeb647af6697
Jarod Kong <1265598443@qq.com>
2021-12-07 17:04:08 +0800

    项目微服务化

--- shardingsphere-ui-backend/src/test/java/cn/com/bluemoon/shardingsphere/backend/SchemaMetadataTest.java
+++ shardingsphere-ui-backend/src/test/java/cn/com/bluemoon/shardingsphere/backend/SchemaMetadataTest.java
@@ -8,7 +8,11 @@ import org.apache.shardingsphere.encrypt.api.config.rule.EncryptColumnRuleConfig
 import org.apache.shardingsphere.encrypt.api.config.rule.EncryptTableRuleConfiguration;
 import org.apache.shardingsphere.infra.config.RuleConfiguration;
 import org.apache.shardingsphere.infra.config.algorithm.ShardingSphereAlgorithmConfiguration;
+import org.apache.shardingsphere.infra.metadata.schema.ShardingSphereSchema;
+import org.apache.shardingsphere.mode.metadata.persist.MetaDataPersistService;
+import org.apache.shardingsphere.mode.metadata.persist.service.SchemaMetaDataPersistService;
 import org.apache.shardingsphere.ui.common.domain.SensitiveInformation;
+import org.apache.shardingsphere.ui.servcie.CenterConfigService;
 import org.apache.shardingsphere.ui.servcie.ConfigCenterService;
 import org.apache.shardingsphere.ui.servcie.ShardingSchemaService;
 import org.junit.Test;
@@ -28,8 +32,16 @@ public class SchemaMetadataTest extends BaseTest{
     @Autowired
     private ShardingSchemaService schemaService;
 
+    @Autowired
+    private ConfigCenterService configCenterService;
+
     @Test
     public void name() {
+        MetaDataPersistService metadataService = configCenterService.getActivatedMetadataService();
+        SchemaMetaDataPersistService schemaMetaDataService = metadataService.getSchemaMetaDataService();
+        Optional<ShardingSphereSchema> sphereSchema = schemaMetaDataService.load("ec_order_db");
+        ShardingSphereSchema shardingSphereSchema = sphereSchema.get();
         Collection<String> allSchemaNames = schemaService.getAllSchemaNames();
+
     }
 }
