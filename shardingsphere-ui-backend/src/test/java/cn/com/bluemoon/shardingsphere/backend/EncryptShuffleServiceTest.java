192721775398317927fa45a059eb11abd1ceda1c
Jarod Kong <1265598443@qq.com>
2021-12-28 20:47:36 +0800

    feat: 入参base64

--- shardingsphere-ui-backend/src/test/java/cn/com/bluemoon/shardingsphere/backend/EncryptShuffleServiceTest.java
+++ shardingsphere-ui-backend/src/test/java/cn/com/bluemoon/shardingsphere/backend/EncryptShuffleServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Collections;
+import java.util.HashMap;
 
 /**
  * @author Jarod.Kong
@@ -15,6 +16,10 @@ public class EncryptShuffleServiceTest extends BaseTest {
 
     @Test
     public void testSubmit() {
-        encryptShuffleService.submitJob("ec_order_test", Collections.singleton("ec_oms_invoice"));
+//        encryptShuffleService.submitJob("ec_order", null, null);
+//        encryptShuffleService.submitJob("ec_order",null, null);
+        encryptShuffleService.submitJob("ec_order", Collections.singleton("sys_user"), new HashMap<String, String>(){{
+            put("sys_user", "2021-12-20 11:19:33");
+        }});
     }
 }
