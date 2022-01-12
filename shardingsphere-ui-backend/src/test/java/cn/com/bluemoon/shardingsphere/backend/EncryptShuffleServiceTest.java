959e79a0d7aa8134c177ed11d35c2af0374cbcd1
Jarod Kong <1265598443@qq.com>
2022-01-12 10:49:17 +0800

    提交单元测试洗数

--- shardingsphere-ui-backend/src/test/java/cn/com/bluemoon/shardingsphere/backend/EncryptShuffleServiceTest.java
+++ shardingsphere-ui-backend/src/test/java/cn/com/bluemoon/shardingsphere/backend/EncryptShuffleServiceTest.java
@@ -4,8 +4,8 @@ import org.apache.shardingsphere.ui.servcie.EncryptShuffleService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * @author Jarod.Kong
@@ -16,10 +16,18 @@ public class EncryptShuffleServiceTest extends BaseTest {
 
     @Test
     public void testSubmit() {
-        encryptShuffleService.submitJob("ec_order", Collections.singleton("ec_oms_order"),null, null);
-//        encryptShuffleService.submitJob("ec_order",Collections.singleton("ec_oms_order"), null, null);
-//        encryptShuffleService.submitJob("ec_order", null, Collections.singleton("ec_oms_order"), new HashMap<String, String>(){{
-//            put("ec_oms_order", "2021-01-05 10:00:00");
-//        }});
+        encryptShuffleService.submitJob("ec_order", new HashSet<String>() {{
+                    add("ec_oms_order");
+                    add("ec_oms_plat_order_encrypt_data");
+                }},
+                null, null);
+        encryptShuffleService.submitJob("ec_order", null, new HashSet<String>() {{
+                    add("ec_oms_order");
+                    add("ec_oms_plat_order_encrypt_data");
+                }},
+                new HashMap<String, String>() {{
+                    put("ec_oms_order", "2022-01-03 10:00:00");
+                    put("ec_oms_plat_order_encrypt_data", "2022-01-03 10:00:00");
+                }});
     }
 }
