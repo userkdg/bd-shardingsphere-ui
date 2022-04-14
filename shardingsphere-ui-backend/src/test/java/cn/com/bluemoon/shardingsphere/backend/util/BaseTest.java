55fab892e78b689765076a87c02ff12e1b74885a
Jarod Kong <1265598443@qq.com>
2022-04-14 11:48:41 +0800

    feat:改造导入模板

--- shardingsphere-ui-backend/src/test/java/cn/com/bluemoon/shardingsphere/backend/util/BaseTest.java
+++ shardingsphere-ui-backend/src/test/java/cn/com/bluemoon/shardingsphere/backend/util/BaseTest.java
@@ -1,25 +1,14 @@
 package cn.com.bluemoon.shardingsphere.backend.util;
 
-import org.apache.commons.lang.RandomStringUtils;
 import org.apache.shardingsphere.ui.Bootstrap;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Random;
-
 /**
  * @author Jarod.Kong
  */
-/*@SpringBootTest(classes = Bootstrap.class)
-@RunWith(value = SpringRunner.class)*/
+@SpringBootTest(classes = Bootstrap.class)
+@RunWith(value = SpringRunner.class)
 public class BaseTest {
-
-    @Test
-    public void test(){
-
-        String filename = RandomStringUtils.randomAlphanumeric(16);
-        System.out.println(filename);
-    }
 }
