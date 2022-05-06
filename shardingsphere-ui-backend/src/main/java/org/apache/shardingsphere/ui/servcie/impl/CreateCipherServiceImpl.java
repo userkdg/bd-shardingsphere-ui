bf22c194667ac5c1b7c709d2ec896efec467acab
Jarod Kong <1265598443@qq.com>
2022-05-06 15:34:18 +0800

    feat:art:脚本生成 1.生成创建密文列索引语句 2.生成备份列删除、可空语句 3.生成密文列非空语句

--- shardingsphere-ui-backend/src/main/java/org/apache/shardingsphere/ui/servcie/impl/CreateCipherServiceImpl.java
+++ shardingsphere-ui-backend/src/main/java/org/apache/shardingsphere/ui/servcie/impl/CreateCipherServiceImpl.java
@@ -11,6 +11,7 @@ import com.baomidou.mybatisplus.annotation.DbType;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.compress.utils.Lists;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.shardingsphere.encrypt.api.config.EncryptRuleConfiguration;
 import org.apache.shardingsphere.encrypt.api.config.rule.EncryptColumnRuleConfiguration;
 import org.apache.shardingsphere.encrypt.api.config.rule.EncryptTableRuleConfiguration;
@@ -72,17 +73,16 @@ public class CreateCipherServiceImpl implements CreateCipherService {
         Collection<DataSourceConfiguration> values = load.values();
         // 只需获取第一个数据源
         Optional<DataSourceConfiguration> first = values.stream().findFirst();
-        Map<QueryMetaDataRequest, List<TableInfoVO>> map = new HashMap<>();
+        Map<QueryMetaDataRequest, List<TableInfoVO>> dataSourceMetadata = new HashMap<>();
         if(first.isPresent()){
-            getMetaInfo(first.get(), names, map);
+            getMetaInfo(first.get(), names, dataSourceMetadata);
         }else {
             return ResponseResult.error("数据源不存在");
         }
         // 筛选表字段
-        if(!map.isEmpty()){
+        if(!dataSourceMetadata.isEmpty()){
             AbsScreenTableFactory absScreenTableFactory = AbsScreenTableFactory.screenMysqlFieldFactory();
-            List<ColumnInfoVO> fieldList = absScreenTableFactory.screenSameField(map, tables, isCipher);
-            QueryMetaDataRequest request = map.keySet().stream().findFirst().get();
+            List<ColumnInfoVO> fieldList = absScreenTableFactory.screenSameField(dataSourceMetadata, tables, isCipher);
             FieldFactory mysqlFieldFactory = FieldFactory.mysqlFieldFactory(DbType.MYSQL);
             List<String> sqlList = null;
             // 密文字段处理
@@ -91,6 +91,9 @@ public class CreateCipherServiceImpl implements CreateCipherService {
                 // 创建sql
                 sqlList = cipherInfo.stream().map(l -> mysqlFieldFactory.createCipherFieldSql(l)).collect(Collectors.toList());
                 generateTempFile(schemaName + "_1-1_创建密文列_", sqlList);
+                // 创建密文列相关索引（明文列对应的索引）
+                sqlList = mysqlFieldFactory.createCipherIndexes(dataSourceMetadata, fieldList, encryptors);
+                generateTempFile(schemaName + "_1-2_创建密文列索引_", sqlList);
             }else if(!isCipher && !fieldList.isEmpty()) {
                 // 明文列改为明文备份列、密文列改为明文列
                 sqlList = fieldList.stream().map(f -> mysqlFieldFactory.renamePlainFieldSql(f)).collect(Collectors.toList());
@@ -98,6 +101,7 @@ public class CreateCipherServiceImpl implements CreateCipherService {
                     log.info("gen logic Column to plain sql=>{}", sql);
                 }
                 generateTempFile(schemaName + "_2-1_明文列改为备份列_", sqlList);
+
                 List<FiledEncryptionInfo> cipherInfo = absScreenTableFactory.getCipherInfo(fieldList, encryptors);
                 List<String> renameCipherSqls = cipherInfo.stream().map(l -> mysqlFieldFactory.renameCipherFieldSql(l)).collect(Collectors.toList());
                 for (String sql : renameCipherSqls) {
@@ -105,18 +109,32 @@ public class CreateCipherServiceImpl implements CreateCipherService {
                 }
                 generateTempFile(schemaName + "_2-2_密文列改为明文列_", renameCipherSqls);
                 sqlList.addAll(renameCipherSqls);
+
                 // 生成备份列创建sql
                 List<String> createPlainBakSqls = cipherInfo.stream().map(l -> mysqlFieldFactory.createPlainBakFieldSql(l)).collect(Collectors.toList());
                 for (String sql : createPlainBakSqls) {
                     log.info("gen plain Column bak to logic sql=>{}", sql);
                 }
-                generateTempFile(schemaName+"_3_创建备份列_", createPlainBakSqls);
+                generateTempFile(schemaName+"_3-1_创建备份列_", createPlainBakSqls);
                 // 生成清空备份列数据sql（考虑字段是否存在默认值）
-                List<String> cleanPlainFieldDataSql = cipherInfo.stream().map(l -> mysqlFieldFactory.deletePlainFieldData(l)).collect(Collectors.toList());
+                List<String> cleanPlainFieldDataSql = cipherInfo.stream().map(l -> mysqlFieldFactory.deletePlainFieldSql(l)).collect(Collectors.toList());
                 for (String sql : cleanPlainFieldDataSql) {
-                    log.info("gen clean bak_plain sql=>{}", sql);
+                    log.info("gen delete bak_plain sql=>{}", sql);
                 }
-                generateTempFile(schemaName + "_2-3_备份列数据清空_", cleanPlainFieldDataSql);
+                generateTempFile(schemaName + "_3-2_备份列删除_", cleanPlainFieldDataSql);
+
+                List<String> changePlainFieldNullable = cipherInfo.stream().map(l -> mysqlFieldFactory.changePlainFieldNullable(l)).filter(StringUtils::isNotBlank).collect(Collectors.toList());
+                for (String sql : changePlainFieldNullable) {
+                    log.info("gen bak_plain set nullable sql=>{}", sql);
+                }
+                generateTempFile(schemaName + "_3-3_备份列可空_", changePlainFieldNullable);
+
+                List<String> changeCipherFieldNullable = cipherInfo.stream().map(l -> mysqlFieldFactory.changeCipherFieldSqlNotNull(l)).filter(StringUtils::isNotBlank).collect(Collectors.toList());
+                for (String sql : changeCipherFieldNullable) {
+                    log.info("gen bak_plain set nullable sql=>{}", sql);
+                }
+                generateTempFile(schemaName + "_3-4_密文列非空_", changeCipherFieldNullable);
+
             }else {
                 return ResponseResult.error("字段已创建或不存在");
             }
--- shardingsphere-ui-backend/src/main/java/org/apache/shardingsphere/ui/util/sql/MysqlFieldFactory.java
+++ shardingsphere-ui-backend/src/main/java/org/apache/shardingsphere/ui/util/sql/MysqlFieldFactory.java
@@ -1,17 +1,22 @@
 package org.apache.shardingsphere.ui.util.sql;
 
+import cn.com.bluemoon.metadata.common.enums.IndexTypeEnum;
 import cn.com.bluemoon.metadata.common.enums.JudgeEnum;
+import cn.com.bluemoon.metadata.inter.dto.in.QueryMetaDataRequest;
 import cn.com.bluemoon.metadata.inter.dto.out.ColumnInfoVO;
+import cn.com.bluemoon.metadata.inter.dto.out.FieldInfoVO;
+import cn.com.bluemoon.metadata.inter.dto.out.SqlIndexInfoVO;
+import cn.com.bluemoon.metadata.inter.dto.out.TableInfoVO;
 import cn.hutool.core.util.StrUtil;
 import com.alibaba.excel.util.StringUtils;
 import org.apache.shardingsphere.encrypt.spi.EncryptAlgorithm;
 import org.apache.shardingsphere.infra.config.algorithm.ShardingSphereAlgorithmConfiguration;
 import org.apache.shardingsphere.infra.config.algorithm.ShardingSphereAlgorithmFactory;
 import org.apache.shardingsphere.ui.common.dto.FiledEncryptionInfo;
+import org.springframework.beans.BeanUtils;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class MysqlFieldFactory extends FieldFactory {
 
@@ -50,12 +55,17 @@ public class MysqlFieldFactory extends FieldFactory {
 
     @Override
     public String renamePlainFieldSql(ColumnInfoVO vo) {
+        return renamePlainFieldSql(vo, false);
+    }
+
+    private String renamePlainFieldSql(ColumnInfoVO vo, boolean forceFieldNull) {
         // alter table <表名> change <字段名> <字段新名称> <字段的类型>。
         String length = StringUtils.isBlank(vo.getLength()) ? "" : "(" + vo.getLength() + ")";
         StringBuilder sb = new StringBuilder();
+        String targetFieldName = vo.getName().endsWith("_plain") ? vo.getName() : vo.getName() + "_plain ";
         sb.append("alter table ").append(vo.getTableName())
                 .append(" change ").append(vo.getName())
-                .append(" ").append(vo.getName() + "_plain ")
+                .append(" ").append(targetFieldName).append(" ")
                 .append(vo.getSqlSimpleType()).append(length);
         if (vo.getColumnDefault() != null) {
             String defVal;
@@ -68,14 +78,48 @@ public class MysqlFieldFactory extends FieldFactory {
             }
             sb.append(" default ").append(defVal);
         }
-        if (JudgeEnum.NO.equals(vo.getIsNullable())) {
+        if (JudgeEnum.NO.equals(vo.getIsNullable()) && !forceFieldNull) {
+            // 2022/5/6 备份列生成后，要把非空，都改为可空，原因：该字段已无效，不应影响业务正常插入数据。
             sb.append(" not null ");
+        } else {
+            sb.append(" null ");
         }
         if (vo.getComment() != null) {
             sb.append(" comment '").append(vo.getComment()).append("';");
         }
 //        String rename = String.format("alter table %s change %s %s_plain %s%s default null comment '%s';",
 //                vo.getTableName(), vo.getName(), vo.getName(), vo.getSqlSimpleType(), length, vo.getComment());
+        return sb.toString();
+    }
+
+    private String changeFieldSqlNotNull(ColumnInfoVO vo, boolean forceFieldNull) {
+        // alter table <表名> change <字段名> <字段新名称> <字段的类型>。
+        String length = StringUtils.isBlank(vo.getLength()) ? "" : "(" + vo.getLength() + ")";
+        StringBuilder sb = new StringBuilder();
+        sb.append("alter table ").append(vo.getTableName())
+                .append(" change ").append(vo.getName())
+                .append(" ").append(vo.getName()).append(" ")
+                .append(vo.getSqlSimpleType()).append(length);
+        if (vo.getColumnDefault() != null) {
+            String defVal;
+            if ("".equals(vo.getColumnDefault())) {
+                defVal = "''";
+            } else if (intList.contains(vo.getSqlSimpleType())) {
+                defVal = vo.getColumnDefault();
+            } else {
+                defVal = String.format("'%s'", vo.getColumnDefault());
+            }
+            sb.append(" default ").append(defVal);
+        }
+        if (JudgeEnum.NO.equals(vo.getIsNullable()) && !forceFieldNull) {
+            // 2022/5/6 备份列生成后，要把非空，都改为可空，原因：该字段已无效，不应影响业务正常插入数据。
+            sb.append(" not null ");
+        } else {
+            sb.append(" null ");
+        }
+        if (vo.getComment() != null) {
+            sb.append(" comment '").append(vo.getComment()).append("';");
+        }
         return sb.toString();
     }
 
@@ -149,5 +193,91 @@ public class MysqlFieldFactory extends FieldFactory {
         }
         sb.append(defVal).append(" where 1=1;");
         return sb.toString();
+    }
+
+    @Override
+    public String deletePlainFieldSql(FiledEncryptionInfo info) {
+        ColumnInfoVO vo = info.getColumnInfoVO();
+        StringBuilder sb = new StringBuilder();
+        sb.append("alter table ").append(vo.getTableName())
+                .append(" drop column ").append(vo.getName()).append("_plain;");
+        return sb.toString();
+    }
+
+    /**
+     * 备份列非空改为可空
+     */
+    @Override
+    public String changePlainFieldNullable(FiledEncryptionInfo info) {
+        ColumnInfoVO vo = info.getColumnInfoVO();
+        if (JudgeEnum.YES.equals(vo.getIsNullable())) {
+            return "";
+        }
+        ColumnInfoVO columnInfoVO = new ColumnInfoVO();
+        BeanUtils.copyProperties(vo, columnInfoVO);
+        if (!columnInfoVO.getName().endsWith("_plain")) {
+            columnInfoVO.setName(columnInfoVO.getName() + "_plain");
+        }
+        return renamePlainFieldSql(columnInfoVO, true);
+    }
+
+    @Override
+    public List<String> createCipherIndexes(Map<QueryMetaDataRequest, List<TableInfoVO>> dataSourceMetadata, List<ColumnInfoVO> needCipherFields, List<Map<String, ShardingSphereAlgorithmConfiguration>> encryptors) {
+        List<TableInfoVO> tableInfoVOS = dataSourceMetadata.values().stream().findFirst().orElse(Collections.emptyList());
+        Map<String, List<SqlIndexInfoVO>> tableNameAndIndexes = tableInfoVOS.stream().collect(Collectors.toMap(TableInfoVO::getName, TableInfoVO::getSqlIndexes));
+        Map<String, List<ColumnInfoVO>> tableNameAndColInfo = needCipherFields.stream().collect(Collectors.groupingBy(ColumnInfoVO::getTableName));
+        List<String> res = new ArrayList<>();
+        for (Map.Entry<String, List<ColumnInfoVO>> entry : tableNameAndColInfo.entrySet()) {
+            List<ColumnInfoVO> cipherColumns = entry.getValue();
+            String tableName = entry.getKey();
+            if (tableNameAndIndexes.containsKey(tableName)) {
+                List<SqlIndexInfoVO> indexes = tableNameAndIndexes.getOrDefault(tableName, Collections.emptyList());
+                Set<String> cipherFields = cipherColumns.stream().map(ColumnInfoVO::getName).collect(Collectors.toSet());
+                List<SqlIndexInfoVO> hadCipherFieldIndexes = indexes.stream().peek(i -> {
+                    List<FieldInfoVO> fieldInfoVOS = i.getFields().stream().peek(fieldInfoVO -> {
+                        String f = fieldInfoVO.getName();
+                        // 修正字段名。
+                        if (f.endsWith("_plain")) {
+                            fieldInfoVO.setName(f.substring(0, f.lastIndexOf("_plain")));
+                        } else {
+                            fieldInfoVO.setName(f);
+                        }
+                    }).collect(Collectors.toList());
+                    i.setFields(fieldInfoVOS);
+                }).filter(i -> {
+                    List<String> metadataIndexFields = i.getFields().stream().map(f -> f.getName()).collect(Collectors.toList());
+                    return metadataIndexFields.stream().anyMatch(cipherFields::contains);
+                }).collect(Collectors.toList());
+                if (!hadCipherFieldIndexes.isEmpty()) {
+                    List<String> indexDdl = hadCipherFieldIndexes.stream().map(i -> {
+                        return String.format("alter table %s add %s index %s(%s);",
+                                tableName, IndexTypeEnum.NORMAL.equals(i.getIdxType()) ? "" : i.getIdxType(),
+                                i.getName(), i.getFields().stream().map(f -> {
+                                    String filedName = f.getName();
+                                    if (f.getSubPart() != null) {
+                                        return String.format("%s(%d)", filedName, f.getSubPart());
+                                    }
+                                    return filedName;
+                                }).map(f -> {
+                                    if (cipherFields.contains(f) && !f.endsWith("_cipher")) {
+                                        return f + "_cipher";
+                                    }
+                                    return f;
+                                }).collect(Collectors.joining(", ")));
+                    }).collect(Collectors.toList());
+                    res.addAll(indexDdl);
+                }
+            }
+        }
+        return res;
+    }
+
+    @Override
+    public String changeCipherFieldSqlNotNull(FiledEncryptionInfo info) {
+        ColumnInfoVO vo = info.getColumnInfoVO();
+        if (vo.getIsNullable().equals(JudgeEnum.YES)) {
+            return "";
+        }
+        return changeFieldSqlNotNull(vo, false);
     }
 }
